9a8fc0ae187120c18a622669379cac48169c5b61
Raphael Barreto <rfsbarreto@gmail.com>
2013-11-16 18:39:53 -0300

    Correcao do tratamento do arquivo de entrada

--- nuvemCinzas.py
+++ nuvemCinzas.py
@@ -1,12 +1,11 @@
 from sys import stdin
 
 readline = stdin.readlines()
-for i in xrange(len(readline)):
-	readline[i]=readline[i].split()
-print(readline)
-lines = int(readline[0][0])
-columns = int(readline[0][1])
 data= readline[1:]
+
+lines = int(readline[0].split()[0])
+columns = int(readline[0].split()[1])
+
 
 print(data)
 
@@ -16,9 +15,10 @@ def getVectorPositions(data):
 	aeroporto,nuvem=[],[]	
 	for i in xrange(lines):
 		for j in xrange(columns):
+			#print i,data[0][1],j
 			if data[i][j]=='A' or data[i][j]=='A\n':
 				aeroporto.append(i*columns+j)
-			elif data[i][j]=='*' or data[i][j]=='*\n':
+			elif data[i][j]=='#' or data[i][j]=='#\n':
 				nuvem.append(i*columns+j)
 	return [aeroporto,nuvem]
 
